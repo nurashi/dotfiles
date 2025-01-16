472d635bd50ed7ef71494be50b1f2f656eca22d8
Abzal Orazbek <abzalorazbek2003@gmail.com>
2025-01-16 10:18:18 +0500

    Add keybind for explorer

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -64,6 +64,9 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Explorer
+vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
