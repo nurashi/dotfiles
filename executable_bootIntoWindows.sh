f4fcc41ff209134a9403969309dc3174bb47e5a6
Abzal Orazbek <abzalorazbek2003@gmail.com>
2025-02-12 14:13:14 +0500

    Reconfigure monitors for reinstall of Arch

--- executable_bootIntoWindows.sh
+++ executable_bootIntoWindows.sh
@@ -1,4 +1,4 @@
 # Problem is that grub's save-default won't work when you reboot in Windows
 # You have to manually select one to save it as a default
-sudo grub-reboot 4
+sudo grub-reboot 2
 sudo reboot
